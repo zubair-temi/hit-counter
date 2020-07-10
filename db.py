cb8e165fc6046420aaf5402d8db1ab1a7715708b
Saqib Ahmed <saqibahmed515@gmail.com>
2020-07-11 04:27:25 +0500

    Add postgres support

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 import sqlite3 as lite
 import config
+import psycopg2
+import urllib.parse as urlparse
+import os
 from collections import defaultdict
 import re
 
@@ -8,21 +11,61 @@ class DbAccess:
     """ This provides access to the database to keep track of urls and views """
     def __init__(self, filename):
         """ Setup connection to file and create tables if they don't exist"""
-        self.filename = filename
-        connection = lite.connect(filename)
-        connection.execute('pragma journal_mode=wal')
-        cursor = connection.cursor()
-        cursor.execute('CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY, url VARCHAR(256), count INTEGER);')
+        try:
+            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+            dbname = url.path[1:]
+            user = url.username
+            password = url.password
+            host = url.hostname
+            port = url.port
+
+            connection = psycopg2.connect(
+                        dbname=dbname,
+                        user=user,
+                        password=password,
+                        host=host,
+                        port=port
+                        )
+
+            cursor = connection.cursor()
+            cursor.execute('CREATE TABLE IF NOT EXISTS url (id SERIAL PRIMARY KEY, url VARCHAR(256), count INTEGER);')
+            connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error :
+            print ("Error while creating PostgreSQL table", error)
+        finally:
+            #closing database connection.
+                if(connection):
+                    cursor.close()
+                    connection.close()
+                    print("PostgreSQL connection is closed")
+
+
+    def parse_db_url(self):
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+
+        return (dbname, user,password,host,port)
 
     def get_connection(self):
         """ Get the cursor to use in the current thread and remove rows that have expired in views"""
-        connection = lite.connect(self.filename)
+        db_attr = self.parse_db_url()
+        connection = psycopg2.connect(
+                        dbname=db_attr[0],
+                        user=db_attr[1],
+                        password=db_attr[2],
+                        host=db_attr[3],
+                        port=db_attr[4]
+                        )
         return connection
 
     def getCount(self, connection, url):
         """ Get the count of a particular url """
         cursor = connection.cursor()
-        cursor.execute('SELECT count FROM url WHERE url=?', (url,))
+        cursor.execute('SELECT count FROM url WHERE url=%s', (url,))
         data = cursor.fetchone()
         if data is None:
             return 0
@@ -35,9 +78,9 @@ class DbAccess:
         # Make sure the url entry exists
         count = self.getCount(connection, url)
         if count == 0:
-            cursor.execute('INSERT INTO url(url, count) VALUES(?, ?)', (url, 0))
+            cursor.execute('INSERT INTO url(url, count) VALUES(%s, %s)', (url, 0))
         # Add 1 to the url count
-        cursor.execute('UPDATE url SET count = count + 1 WHERE url=?', (url, ))
+        cursor.execute('UPDATE url SET count = count + 1 WHERE url=%s', (url, ))
         connection.commit()
 
     def getTopSites(self, connection, amount=10):
